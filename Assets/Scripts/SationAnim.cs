18f3d7ef9e58dfed37f2ae0ab574da5c8644acb4
Daniel <daniel67liu@gmail.com>
2021-10-25 01:26:14 -0400

    Station Anim added. Train and water.

---
+++ Assets/Scripts/SationAnim.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SationAnim : MonoBehaviour
+{
+    
+    public GameObject Train;
+    public GameObject Water;
+    
+
+    public void AnimBegin()
+    {
+        Train.gameObject.SetActive(true);
+        Water.gameObject.SetActive(true);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
